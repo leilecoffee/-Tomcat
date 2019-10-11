abce192ab57f695fcadddef1069ee090f130de03
leilecoffee <xwm1009@126.com>
2019-10-11 16:19:32 +0800

    一个简单的web服务器

---
+++ ch01/src/ex01/HttpServer.java
@@ -0,0 +1,62 @@
+package ex01;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+	public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+	private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+	private boolean shutdown = false;
+
+	public static void main(String[] args) {
+		HttpServer server = new HttpServer();
+		server.await();
+	}
+
+	public void await() {
+		ServerSocket serverSocket = null;
+		//监听的端口
+		int port = 8080;
+		try {
+			//操作系统构建一个队列来存放请求的连接，先入先出
+			serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+		} catch (Exception e) {
+			System.exit(1);
+		}
+		while (!shutdown) {
+			Socket socket = null;
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				//从队列中获取连接,如果队列中没有连接则会阻塞
+				socket = serverSocket.accept();
+				in = socket.getInputStream();
+				out = socket.getOutputStream();
+
+				Request request = new Request(in);
+				//解析请求的报文
+				request.parse();
+				Response response = new Response(out);
+				response.setRequest(request);
+				//发送基于HTTP协议的响应
+				response.sendStaticResouce();
+				//关闭连接
+				socket.close();
+				if (request.getUri() != null)
+					shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
